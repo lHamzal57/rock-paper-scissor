c8522541a304e6e8e05268dbd5777a8b77444358
Mohamed Hamza <mohamed.hamza5796@gmail.com>
2024-09-18 18:18:31 +0300

    configured a new model for Player with property of playerName

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,6 @@
 package org.game
 
+import org.game.org.game.domain.model.Player
 import org.game.org.game.domain.usecase.PlayGameUseCase
 
 fun main() {
@@ -8,10 +9,13 @@ fun main() {
 
     val rounds = 10_000
 
+    val player1 = Player("Rena")
+    val player2 = Player("Big")
+
     val result = rockPaperScissorsGame.playGame(rounds)
     println("""
-        Player A wins ${result.playerAWins} of $rounds games
-        Player B wins ${result.playerBWins} of $rounds games
-        Draws: ${result.draws} of $rounds games"""
+        Player ${player1.playerName} wins ${result.playerAWins} of $rounds games
+        Player ${player2.playerName} wins ${result.playerBWins} of $rounds games
+        Draws: ${result.draws} of $rounds games""".trimIndent()
     )
 }
---
+++ src/main/kotlin/org/game/domain/model/Player.kt
@@ -0,0 +1,5 @@
+package org.game.org.game.domain.model
+
+data class Player(
+    val playerName: String
+)
